6418533be27e92ea55c589d10985b49707f1eed1
Anish Dandekar <dAN1Sh@Anish-Dandekar.local>
2020-12-22 11:18:54 +0530

    house listing

--- src/components/HouseListing/HouseListings.js
+++ src/components/HouseListing/HouseListings.js
@@ -3,74 +3,66 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 const HouseListings = () => {
   return (
-    <View
-      style={{
-        margin: 10,
-      }}
-    >
+    <View style={styles.container}>
       <Image
         source={{
           uri:
             "https://i0.wp.com/hooquest.com/wp-content/uploads/2019/08/indy-home-pros-listing-description.jpg?resize=750%2C343&ssl=1",
         }}
-        style={{
-          width: "100%",
-          aspectRatio: 3 / 2,
-          resizeMode: "cover",
-          borderRadius: 10,
-        }}
+        style={styles.image}
       />
-      <Text
-        style={{
-          marginVertical: 10,
-          fontSize: 14,
-          color: "gray",
-        }}
-      >
-        1 bed 1 bedroom
-      </Text>
-      <Text
-        style={{
-          fontSize: 18,
-          lineHeight: 26,
-        }}
-        numberOfLines={2}
-      >
+      <Text style={styles.roomAvailabiltyText}>1 bed 1 bedroom</Text>
+      <Text style={styles.description} numberOfLines={2}>
         lSpacious is an adjective that means "roomy" or "having plenty of room."
         The adjective spacious is most commonly used to describe living quarters
         that have plenty of space, but it can be used to describe other things
         as well.
       </Text>
 
-      <Text
-        style={{
-          fontSize: 18,
-          marginVertical: 10,
-        }}
-      >
-        <Text
-          style={{
-            color: "gray",
-            textDecorationLine: "line-through",
-          }}
-        >
-          $36
-        </Text>
-        <Text style={{ fontWeight: "bold" }}> $30</Text>
+      <Text style={styles.prices}>
+        <Text style={styles.oldPrice}>$36</Text>
+        <Text style={styles.revisedPrice}> $30</Text>
         /night
       </Text>
-      <Text
-        style={{
-          color: "gray",
-          textDecorationLine: "underline",
-        }}
-      >
-        $230 total
-      </Text>
+      <Text style={styles.total}>$230 total</Text>
     </View>
   );
 };
 
 export default HouseListings;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 3 / 2,
+    resizeMode: "cover",
+    borderRadius: 10,
+  },
+  roomAvailabiltyText: {
+    marginVertical: 10,
+    fontSize: 14,
+    color: "gray",
+  },
+  description: {
+    fontSize: 18,
+    lineHeight: 26,
+  },
+  prices: {
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  oldPrice: {
+    color: "gray",
+    textDecorationLine: "line-through",
+  },
+  revisedPrice: {
+    fontWeight: "bold",
+  },
+  total: {
+    color: "gray",
+    textDecorationLine: "underline",
+  },
+});
